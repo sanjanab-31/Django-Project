f64bfe68d48acac227187dadbb3eb3c80db82ee0
Sanju <sanjana.b0831@gmail.com>
2025-05-03 12:10:25 +0530

    modified urls and views

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):    
-     return HttpResponse("Hello, this is the Home Page!")
+     return render(request, 'myapp/months.html')
