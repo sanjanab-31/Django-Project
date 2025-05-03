f15fa306c50eba1e793b5dba0867399aab50ecee
Sanju <sanjana.b0831@gmail.com>
2025-05-03 14:23:14 +0530

    make month clickable

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -7,4 +7,3 @@ def home(request):
      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']  
      
      return render(request, 'myapp/months.html', {'months': months})
-
