ad2a84f3f0a0204024dfdc821b2669adbd32f570
Sanju <sanjana.b0831@gmail.com>
2025-05-03 14:15:28 +0530

    modified html and views

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def home(request):    
-     return render(request, 'myapp/months.html')
+def home(request):  
+     months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']  
+     
+     return render(request, 'myapp/months.html', {'months': months})
+
